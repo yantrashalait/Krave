764598e1d329171d4f09f2a464ad8957a16866a4
sanip <saneprijal@gmail.com>
2019-11-03 13:33:46 +0545

    change in settings

---
+++ krave/local_settings.sample.py
@@ -0,0 +1,36 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = ''
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# uncomment below in development
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# use in deployment
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
