3bc13a667b5ac945292dc6c066271c5a2d6282f3
sanip <saneprijal@gmail.com>
2020-01-06 14:05:18 +0545

    filter foods per restaurant and category

--- core/templatetags/filters.py
+++ core/templatetags/filters.py
@@ -1,8 +1,13 @@
 from django.template import Library
-
+from core.models import FoodMenu
 
 register = Library()
 
 @register.filter
 def space_to_underscore(obj):
     return obj.replace(" ", "_")
+
+
+@register.filter
+def get_food_items(obj, rest):
+    return FoodMenu.objects.filter(category=obj, restaurant=rest)
