01e6447cae64b9af01202c46036d8125ba676ed5
sanip <saneprijal@gmail.com>
2020-01-07 09:41:39 +0545

    context processors fix

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,7 +1,10 @@
 from .models import FoodCart, Order
 
 def get_header_notifs(request):
-    user = request.user
-    cart = FoodCart.objects.filter(user=request.user, checked_out=False)
-    cart_count = FoodCart.objects.filter(user=request.user, checked_out=False).count()
-    return {'cart': cart, 'cart_count': cart_count}
+    if request.user.is_authenticated:
+        user = request.user
+        cart = FoodCart.objects.filter(user=request.user, checked_out=False)
+        cart_count = FoodCart.objects.filter(user=request.user, checked_out=False).count()
+        return {'cart': cart, 'cart_count': cart_count}
+    else:
+        return {'message': 'not authenticated'}
