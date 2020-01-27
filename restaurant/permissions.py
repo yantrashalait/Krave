d1ae4ae79f16ef4153f7edecaed1a48d013c6a9e
sanip <saneprijal@gmail.com>
2020-01-27 23:36:05 +0545

    permissions fix

--- restaurant/permissions.py
+++ restaurant/permissions.py
@@ -2,8 +2,19 @@ from django.core.exceptions import PermissionDenied
 from userrole.models import UserRole
 from core.models import Restaurant
 
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
-class RestaurantAdminMixin(object):
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **kwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**kwargs)
+        return login_required(view)
+
+
+class RestaurantAdminMixin(LoginRequiredMixin):
 
     def dispatch(self, request, *args, **kwargs):
         restaurant_id = kwargs.pop('rest_id')
