193966c92b99d85c0a44841cb5b20ca81aa7b4bd
sanip <saneprijal@gmail.com>
2020-05-05 10:04:28 +0545

    cart list serializer update

--- api/serializers/order.py
+++ api/serializers/order.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
 from core.models import Restaurant, RestaurantCuisine, RestaurantFoodCategory, RestaurantImage, Order, FoodCart, FoodExtra
 from django.db.models import Q
+from api.serializers.food import FoodDetailSerializer
+
+
+class CartListSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.id")
+    added_on = serializers.ReadOnlyField()
+    checked_out = serializers.ReadOnlyField()
+    total_price = serializers.SerializerMethodField(read_only=True)
+    restaurant = serializers.ReadOnlyField(source="restaurant.id")
+    food = FoodDetailSerializer()
+
+    class Meta:
+        model = FoodCart
+        exclude = ("modified_on", "session_key")
+        extra_kwargs = {'number_of_food': {'required': True}, 'style': {'required': True}, 'extras': {'required': False}}
+    
+    def get_total_price(self, obj):
+        return obj.get_total
 
 
 class CartSerializer(serializers.ModelSerializer):
