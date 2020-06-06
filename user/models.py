6a288b8d75afaaf5ed7d9264c29c2a63b51b8df4
sanip <saneprijal@gmail.com>
2020-06-06 11:23:27 +0545

    custom manager to find nearby delivery person

--- user/models.py
+++ user/models.py
@@ -2,6 +2,44 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.contrib.gis.db.models import PointField
+from geopy import units, distance
+
+
+class LocationManager(models.Manager):
+    def near(self, latitude=None, longitude=None, distance_range=10):
+        queryset = super(LocationManager, self).get_query_set()
+        if not (latitude and longitude and distance_range):
+            return queryset.none()
+
+        latitude       = float(latitude)
+        longitude      = float(longitude)
+        distance_range = float(distance_range)
+
+        rough_distance = units.degrees(arcminutes=units.nautical(kilometers=distance_range)) * 2
+
+        queryset = queryset.filter(
+            last_location_point__y__range=(
+                latitude - rough_distance,
+                latitude + rough_distance
+            ),
+            last_location_point__x__range=(
+                longitude - rough_distance,
+                latitude + rough_distance
+            )
+        )
+
+        locations = []
+        for location in queryset:
+            if location.latitude and location.longitude:
+                exact_distance = distance.distance(
+                    (latitude, longitude),
+                    (location.latitude, location.longitude)
+                ).kilometers
+
+                if exact_distance <= distance_range:
+                    locations.append(location)
+        queryset = queryset.filter(id__in=[l.id for l in locations])
+        return queryset
 
 
 class User(AbstractUser):
@@ -34,6 +72,7 @@ class UserProfile(models.Model):
 
 
 class UserLocationTrack(models.Model):
+    location_manager = LocationManager()
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="location")
     last_location_point = PointField(geography=True, srid=4326, blank=True, null=True)
