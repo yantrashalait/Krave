0df575873d58e26106b750cd99a33e639c9ef323
sanip <saneprijal@gmail.com>
2020-06-08 20:03:48 +0545

    fix 'Manager' object has no attribute 'distance'

--- delivery/tasks.py
+++ delivery/tasks.py
@@ -12,27 +12,32 @@ logger = get_task_logger(__name__)
 # return True/False, True/False corresponding to 'Restaurant location not found', 'No user available' respectively.
 @task(name="assign_delivery")
 def assign_delivery(order_id, restaurant_id):
-    logger.info('Assign delivery to a delivery boy')
+    logger.info('Assign delivery to a delivery person')
     try:
         restaurant = Restaurant.objects.get(id=restaurant_id)
     except Restaurant.DoesNotExist:
+        logger.info("No restaurant exists with id " + str(restaurant_id))
         return False, False
     try:
         order = Order.objects.get(id=order_id)
     except Order.DoesNotExist:
+        logger.info("No order exists of specified id " + str(order_id))
         return False, False
 
     restaurant_location = restaurant.location_point
     if not restaurant_location:
+        logger.info("The restaurant does not contain any location. id: " + str(restaurant_id))
         return False, False
 
     else:
         # if there exists a user near the restaurant that is available to delivery
         if User.objects.filter(is_deliveryman=True, is_available=True).exists():
-            user_locations = UserLocationTrack.objects.distance(restaurant_location).filter(user__is_available=True).select_related('user').order_by('last_location_point')
+            user_locations = UserLocationTrack.location_manager.near(longitude=restaurant.longitude, latitude=restaurant.latitude)
             user = user_locations[0].user
             Delivery.objects.create(order=order, delivery_man=user, tracking_code=order.id_string, status=0)
+            logger.info("Delivery person assigned to order")
             return True, True
         # if no user is available
         else:
+            logger.info("No user is available for delivery")
             return True, False
