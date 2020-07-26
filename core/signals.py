fee2962f02acb225c8575d66d1f20edf5e4c3f52
sanip <saneprijal@gmail.com>
2020-07-26 15:05:55 +0545

    send email to user for order actions

--- core/signals.py
+++ core/signals.py
@@ -34,7 +34,7 @@ def order_event_notification(sender, instance, created, **kwargs):
             restaurant = instance.cart.first().restaurant
             userrole = UserRole.objects.get(restaurant=restaurant)
             source_user = userrole.user
-            description = "Your order '" + instance.id_string +"' has been accepted."
+            description = "Your order '" + instance.id_string + "' has been accepted."
             noti = Notification.objects.create(
                 content_object=source_user,
                 order=instance,
@@ -42,6 +42,37 @@ def order_event_notification(sender, instance, created, **kwargs):
                 destination=instance.user,
                 description=description
                 )
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = "Order Placed"
+            msg['From'] = settings.EMAIL_HOST_USER
+            msg['To'] = instance.user.email
+            to_email = instance.user.email
+
+            html = """
+                    <html>
+                        <head></head>
+                        <body>
+                            Greetings """ + instance.user.username + """,
+                            <br>
+                            Your order has been accepted by """ + restaurant.name + """.
+                            <p><b>Order Details</b></p>
+                            <p>
+                                <ul>
+                                    <li><b>Order ID</b>: """ + order.id_string + """"</li>
+                                    <li><b>Order placed on</b>: """ + order.created_at.strftime("%m/%d/%Y, %H:%M:%S") + """</li>
+                                </ul>
+                            </p>
+                        </body>
+                    </html>
+                """
+
+            html_part = MIMEText(html, 'html')
+            msg.attach(html_part)
+
+            server = smtplib.SMTP_SSL(settings.EMAIL_HOST, settings.EMAIL_PORT)
+            server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+            server.sendmail(settings.EMAIL_HOST_USER, [to_email, ], msg.as_string())
+            server.quit()
     except Exception as e:
         print(e)
 
@@ -59,6 +90,38 @@ def order_event_notification(sender, instance, created, **kwargs):
                 description=description
                 )
 
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = "Order Placed"
+            msg['From'] = settings.EMAIL_HOST_USER
+            msg['To'] = instance.user.email
+            to_email = instance.user.email
+
+            html = """
+                    <html>
+                        <head></head>
+                        <body>
+                            Greetings """ + instance.user.username + """,
+                            <br>
+                            Your order has been prepared.
+                            <p><b>Order Details</b></p>
+                            <p>
+                                <ul>
+                                    <li><b>Order ID</b>: """ + order.id_string + """"</li>
+                                    <li><b>Order placed on</b>: """ + order.created_at.strftime("%m/%d/%Y, %H:%M:%S") + """</li>
+                                </ul>
+                            </p>
+                        </body>
+                    </html>
+                """
+
+            html_part = MIMEText(html, 'html')
+            msg.attach(html_part)
+
+            server = smtplib.SMTP_SSL(settings.EMAIL_HOST, settings.EMAIL_PORT)
+            server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+            server.sendmail(settings.EMAIL_HOST_USER, [to_email, ], msg.as_string())
+            server.quit()
+
     except Exception as e:
         print(e)
 
@@ -75,6 +138,38 @@ def order_event_notification(sender, instance, created, **kwargs):
                 destination_id=instance.user.pk,
                 description=description
             )
+
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = "Order Placed"
+            msg['From'] = settings.EMAIL_HOST_USER
+            msg['To'] = instance.user.email
+            to_email = instance.user.email
+
+            html = """
+                <html>
+                    <head></head>
+                    <body>
+                        Greetings """ + instance.user.username + """,
+                        <br>
+                        Your order has been declined by """ + restaurant.name + """.
+                        <p><b>Order Details</b></p>
+                        <p>
+                            <ul>
+                                <li><b>Order ID</b>: """ + order.id_string + """"</li>
+                                <li><b>Order placed on</b>: """ + order.created_at.strftime("%m/%d/%Y, %H:%M:%S") + """</li>
+                            </ul>
+                        </p>
+                    </body>
+                </html>
+            """
+
+            html_part = MIMEText(html, 'html')
+            msg.attach(html_part)
+
+            server = smtplib.SMTP_SSL(settings.EMAIL_HOST, settings.EMAIL_PORT)
+            server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+            server.sendmail(settings.EMAIL_HOST_USER, [to_email, ], msg.as_string())
+            server.quit()
     except Exception as e:
         pass
 
