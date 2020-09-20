06f2ae220a81e312d31b87b24c89ec53e965c3c9
Sakun <napitsakun@gmail.com>
2020-09-20 11:20:17 +0545

    required changes done

--- core/forms.py
+++ core/forms.py
@@ -114,7 +114,7 @@ class FoodMenuForm(forms.ModelForm):
     # category = forms.ChoiceField(widget=forms.Select(attrs={'class': 'fd-ct'}))
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Item Name'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Keep it more descriptive in less words'}))
-    ingredients = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Separate with comma(,)'}))
+    ingredients = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ingredients', 'data-role': 'tagsinput'}))
     old_price = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Old Price'}))
     new_price = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'New Price'}))
     preparation_time = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'E.g. (10-20)'}))
--- static/restaurant/js/main.js
+++ static/restaurant/js/main.js
@@ -1,72 +1,118 @@
-(function ($) {
-  "use strict";
-  $(document).ready(function () {
+(function($) {
+    'use strict';
+    $(document).ready(function(){
+
     function readURL(input) {
       if (input.files && input.files[0]) {
         var reader = new FileReader();
 
-        reader.onload = function (e) {
-          $("#blah").attr("src", e.target.result);
-        };
+        reader.onload = function(e) {
+          $('#blah').attr('src', e.target.result);
+        }
 
         reader.readAsDataURL(input.files[0]);
       }
     }
-    $("#imgInp").change(function () {
+
+    function readImageURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          $('#image_blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    }
+
+    function readFoodURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          $('#food_blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    }
+
+    function readCatURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          $('#cat_image_blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    }
+
+    $("#id_logo").change(function() {
       readURL(this);
     });
 
-    $(".fd-ct").select2({
-      placeholder: "Select a Category",
+    $('#id_img').change(function(){
+      readFoodURL(this);
     });
+
+    $("#id_image").change(function(){
+      readImageURL(this);
+    });
+
+    $("#id_cat_image").change(function(){
+      readCatURL(this);
+    });
+
+    $('.fd-ct').select2({
+        placeholder: "Select a Category"
+    });
+
+
 
     $("#cl-bt").click(function (e) {
-      e.preventDefault();
-      var $self = $(this);
-      var cloneIndex = $(".md-gp-ls").length;
-      $self.before(
-        $self
-          .prev(".md-gp-ls")
-          .clone()
-          .attr("id", "md-gp-ls-id" + cloneIndex)
-      );
+        e.preventDefault();
+        var $self = $(this);
+        var cloneIndex = $(".md-gp-ls").length;
+        $self.before($self.prev('.md-gp-ls').clone().attr("id", "md-gp-ls-id" +  cloneIndex));
     });
 
-    $("#cl-bt-rd").click(function (e) {
-      e.preventDefault();
-      var $self = $(this);
-      var cloneIndexrd = $(".md-gp-ls-rd").length;
-      $self.before(
-        $self
-          .prev(".md-gp-ls-rd")
-          .clone()
-          .attr("id", "md-gp-ls-rd-id" + cloneIndexrd)
-      );
+
+    // $("#cl-bt-rd").click(function (e) {
+    //     e.preventDefault();
+    //     var $self = $(this);
+    //     var cloneIndexrd = $(".md-gp-ls-rd").length;
+    //     $self.before($self.prev('.md-gp-ls-rd').clone().attr("id", "md-gp-ls-rd-id" +  cloneIndexrd));
+    // });
+
+
+    $('#fd-it-ls-dt').DataTable();
+
+    });// document ready
+
+    //checkbox
+    $("#demo").easySelect({
+        showEachItem: true,
+        dropdownMaxHeight: "100px",
     });
 
-    $("#fd-it-ls-dt").DataTable();
-  });
+    //cuisines selection
+    $("#cus_itm").easySelect({
+        search: true,
+        showEachItem: true,
+        placeholder: "Select cuisines type",
+        dropdownMaxHeight: "auto",
+    });
+    //date filter
+    $("#kronos9-1").kronos({
+        periodTo: "#kronos9-2",
+        initDate: "mmddyy",
+    });
+    $("#kronos9-2").kronos({
+        periodFrom: "#kronos9-1",
+        initDate: "mmddyy",
+    });
 
-  //checkbox
-  $("#demo").easySelect({
-    showEachItem: true,
-    dropdownMaxHeight: "100px",
-  });
-
-  //cuisines selection
-  $("#cus_itm").easySelect({
-    search: true,
-    showEachItem: true,
-    placeholder: "Select cuisines type",
-    dropdownMaxHeight: "auto",
-  });
-  //date filter
-  $("#kronos9-1").kronos({
-    periodTo: "#kronos9-2",
-    initDate: "mmddyy",
-  });
-  $("#kronos9-2").kronos({
-    periodFrom: "#kronos9-1",
-    initDate: "mmddyy",
-  });
 })(window.jQuery);
